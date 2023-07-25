29fd5c12457062802f118d7351d5d4087070fd62
Daniel <danielloewito@gmail.com>
2023-07-26 04:11:03 +1000

    echo, var assign, and var substitute is done??

--- sheepy.py
+++ sheepy.py
@@ -1,1 +1,71 @@
-#! /usr/bin/env python3 
+#! /usr/bin/env python3
+
+import sys
+import re
+
+filepath = sys.argv[1]
+
+# * HANDLER HELPERS
+def remove_unescaped_quotes(s):
+    return re.sub(r'(?<!\\)(\'|")', '', s)
+
+def check_var_sub(line):
+    # enclosed by "" translate, otherwise enclosed by '' keep literal
+    if re.search('\$\w+', line):
+        line = re.sub(r'\$(\w+)', r'{\1}', line)
+        for match in re.findall(r"'([^']*)'", line):
+                # Within those substrings, replace {var} back into $var.
+                replaced = re.sub(r'\{(\w+)\}', r'$\1', match)
+
+                # Replace the original substring with the modified one.
+                line = line.replace("'" + match + "'", "'" + replaced + "'")
+
+        line = remove_unescaped_quotes(line)
+        return 'f"' + line + '"'
+    else:
+        line = remove_unescaped_quotes(line)
+        return '"' + line + '"'
+
+# *HANDLERS
+def echo_line(line):
+    line = line.replace('echo ', '', 1)
+
+    str_to_print = ""
+    if line.startswith(("'", '"')) and line.endswith(("'", '"')):       
+        str_to_print = line
+    else:
+        wordarr = re.split('\s+', line)
+        for word in wordarr:
+            str_to_print += word + " "
+        str_to_print = str_to_print.strip()
+
+    str_to_print = check_var_sub(str_to_print)
+    return f'print({str_to_print})'
+
+def var_assign(line):
+    var, value = re.split('=', line)
+    if value.startswith(("'", '"')) and value.endswith(("'", '"')):
+        value = value[1:-1]
+    
+    value = check_var_sub(value)
+    return f'{var} = {value}'
+
+command_handler = {
+    # Match enclosed quotes \'[^\']*\' | \"[^\"]*\"
+    # Match any string of allowed characters without quotes or variable subs \$\w+ | \w+
+    # Match zero or more whitespace to allow space (patterns)\s*
+    r'^echo ((\'[^\']*\'|\"[^\"]*\"|\$\w+|\w+)\s*)+$': echo_line,
+    r'^[\w]+=((\'[^\']*\'|\"[^\"]*\"|\$\w+|\w+)\s*)+$': var_assign
+}
+with open(filepath, 'r') as file:
+    lines = file.readlines()
+
+for line in lines:
+    line = line.strip()
+
+    for pattern, handler in command_handler.items():
+        if re.match(pattern, line):
+            print(handler(line))
+            break
+    else:
+        print()
---
+++ test1.sh
@@ -0,0 +1,26 @@
+#!/bin/dash
+
+theAnswer=42
+echo meaning of life bla bla                    blaaaaaaa
+echo "b ajs fd                d"
+
+name=COMP2041
+echo I BHEOPASF 
+
+gotcha='ec/.390@/*_ho'
+gotcha="ec/.390\'@/*_ho"
+gotcha=testoa2990_
+echo $gotcha
+
+H=Hello
+W=World
+echo $H, $W
+
+test=$H
+P1=race
+P2=car
+
+palindrome="test $P1$P2"
+echo toss $palindrome
+echo 'toss' $palindrome
+echo '$palindrome'toss" $palindrome"
