ce20daed051bdc0e4ea2c6c9d2cf108bd8701201
Daniel <danielloewito@gmail.com>
2023-07-28 01:05:07 +1000

    ge

--- sheepy.py
+++ sheepy.py
@@ -2,13 +2,6 @@
 import sys
 import re
 
-# TODO: SHEBANG LINE
-
-#* File structure
-# sheepy.py (command_handler and fileread. Import all the functions in command handler)
-# subset_0.py (handlers and helpers for subset 0)
-# subset_1.py (take a guess)
-
 # * HANDLER HELPERS
 def remove_start_end_quotes(s):
     if (s.startswith("'") and s.endswith("'")) or (s.startswith('"') and s.endswith('"')):
@@ -17,7 +10,6 @@ def remove_start_end_quotes(s):
         return s
 
 # *HANDLERS
-
 # SUBSET 0
 # $
 def var_sub(variables, line):
@@ -33,7 +25,8 @@ def var_sub(variables, line):
 # echo
 def echo_line(variables, line):
     line = line.replace('echo ', '', 1)
-
+    no_end_quote = False
+    no_start_quote = False
     str_to_print = ""
     if line.startswith(("'")) and line.endswith(("'")):       
         str_to_print = line
@@ -44,9 +37,43 @@ def echo_line(variables, line):
             str_to_print += word + " "
         str_to_print = str_to_print.strip()
 
+    str_to_print = remove_start_end_quotes(str_to_print)    
+
+    # VARIABLE SUBSTITUTION
     str_to_print = var_sub(variables, str_to_print)
-    str_to_print = remove_start_end_quotes(str_to_print)
-    return f'print("{str_to_print}")'
+
+    # GLOBBING
+
+
+    globs = behold_the_glob(str_to_print)
+    # given gjealk glob DLKfkj
+    # replace glob at start with glob + "rest of string"
+    # replace glob at end with "rest of string" + glob
+    # replace glob at middle with "first" + glob + "Seoncd"
+    # replace glob at start and end with glob
+    for key in globs.keys():
+        replacement = globs[key]
+        if str_to_print == key:
+            # ONLY
+            str_to_print = str_to_print.replace(key, replacement)
+            no_end_quote = True
+            no_start_quote = True
+        elif str_to_print.startswith(key):
+            # STARTS
+            str_to_print = str_to_print.replace(key, replacement + " + \"")
+            no_start_quote = True
+        elif str_to_print.endswith(key):
+            # ENDS
+            str_to_print = str_to_print.replace(key, "\" + " + replacement)
+            no_end_quote = True
+        else:
+            #MIDDLE
+            str_to_print = str_to_print.replace(key, "\" + " + replacement + " + \"")
+    
+    end_quote = "" if no_end_quote else "\""
+    start_quote = "" if no_start_quote else "\""
+
+    return f'print({start_quote}{str_to_print}{end_quote})'
 
 # =
 def var_assign(variables, line):
@@ -74,7 +101,27 @@ def inline_comments(line):
         return content, comment
     else:
         return line, ""
+    
+# SUBSET 1
+# globbing
+def behold_the_glob(line):
+    # look for word containing *, ?, [, and ]
 
+    # return word: replacement
+    # go replace it and deal with it in the adult function
+    result=  {}
+    pattern = r'(?:^|\s)(\S*[\*\?\[\]]+\S*)(?:\s|$)'
+
+    # Look for matches in the line
+    matches = re.findall(pattern, line)
+
+    # For each match, replace it appropriately
+    for match in matches:
+        result[match] = f'"".join(sorted(glob.glob("{match}")))'
+
+    return result
+
+# *COMMAND HANDLER
 filepath = sys.argv[1]
 command_handler = {
     # Match enclosed quotes \'[^\']*\' | \"[^\"]*\"
@@ -87,6 +134,8 @@ command_handler = {
     r'^[\w]+=.+': lambda line: var_assign(variables, line)
 }
 
+# *MAIN LOOP
+imports = []
 variables = {}
 
 with open(filepath, 'r') as file:
---
+++ test03.sh
@@ -0,0 +1,12 @@
+#!/bin/dash
+
+echo *
+
+C_files=*.[ch]
+echo $C_files
+
+echo middle ?.py glob
+echo ?.py start glob
+echo end glob ?.py
+
+echo multi ?.py ?.py * glob
