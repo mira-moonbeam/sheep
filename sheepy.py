98bf8737456ac72e17bc4c3959be1f9c39399baf
Daniel <danielloewito@gmail.com>
2023-07-28 02:18:30 +1000

    todo: indentation within for loop

--- sheepy.py
+++ sheepy.py
@@ -15,12 +15,32 @@ def remove_start_end_quotes(s):
 def var_sub(variables, line):
     no_single_quotes = re.sub(r"'[^']*'", "", line)  # remove all parts within single quotes
     matches = re.findall(r"\$\w+", no_single_quotes)  # find all $words in the remaining part
-    
+    no_end_quote = False
+    no_start_quote = False
+
     for match in matches:       
         varName = match[1:]
-        line = line.replace(match, variables[varName])
+        if bool(re.match('^loopvar:.+$', variables[varName])):
+            if line == ("$" + varName):
+                # ONLY
+                line = line.replace(match, varName)
+                no_end_quote = True
+                no_start_quote = True
+            elif line.startswith(("$" + varName)):
+                # STARTS
+                line = line.replace(match, varName + " + \"")
+                no_start_quote = True
+            elif line.endswith(("$" + varName)):
+                # ENDS
+                line = line.replace(match, "\" + " + varName)
+                no_end_quote = True
+            else:
+                #MIDDLE
+                line = line.replace(match, "\" + " + varName + " + \"")
+        else:
+            line = line.replace(match, variables[varName])
 
-    return line 
+    return {'line': line, 'end': no_end_quote, 'start': no_start_quote}
 
 # echo
 def echo_line(variables, line):
@@ -37,10 +57,12 @@ def echo_line(variables, line):
             str_to_print += word + " "
         str_to_print = str_to_print.strip()
 
-   
-
     # VARIABLE SUBSTITUTION
-    str_to_print = var_sub(variables, str_to_print)
+
+    results = var_sub(variables, str_to_print)
+    str_to_print = results['line']
+    no_end_quote = results['end']
+    no_start_quote = results['start']
 
     # GLOBBING
 
@@ -81,8 +103,8 @@ def var_assign(variables, line):
     var, value = re.split('=', line)
 
     if variables:
-        value = var_sub(variables, value)
-
+        results = var_sub(variables, value)
+        value = results['line']
     value = remove_start_end_quotes(value)
 
     variables[var] = value
@@ -122,6 +144,36 @@ def behold_the_glob(line):
 
     return result
 
+def for_loop(variables, start_line):
+    loop_variables = variables.copy()
+    
+    pattern = r'for\s+(\w+)\s+in\s+(.*)'
+
+    match = re.match(pattern, start_line)
+    if match:
+        variable_name = match.group(1)
+        values = match.group(2)
+        loop_variables[variable_name] = "loopvar:"+variable_name
+    else:
+        return "Unrecognized command"
+
+    print(f'for {variable_name} in {values} but pythoned')
+
+    for line in file:
+        line = line.strip()
+        if line == "done":
+            return "done"
+        
+        for pattern, handler in command_handler.items():
+            if re.match(pattern, line):
+                print_line = handler(loop_variables, line) + comment
+                print(print_line)
+                break
+        else:
+            print(line)
+
+    return
+
 # *COMMAND HANDLER
 filepath = sys.argv[1]
 command_handler = {
@@ -131,8 +183,9 @@ command_handler = {
     #r'^echo ((\'[^\']*\'|\"[^\"]*\"|\$\w+|\w+|#)\s*)+$': echo_line,
     #r'^[\w]+=((\'[^\']*\'|\"[^\"]*\"|\$\w+|\w+|#)\s*)+$': var_assign
 
-    r'^echo .+$': lambda line: echo_line(variables, line),
-    r'^[\w]+=.+': lambda line: var_assign(variables, line)
+    r'^echo .+$': echo_line,
+    r'^[\w]+=.+': var_assign,
+    r'^for \w+ in .*': for_loop,
 }
 
 # *MAIN LOOP
@@ -140,18 +193,16 @@ imports = []
 variables = {}
 
 with open(filepath, 'r') as file:
-    lines = file.readlines()
+    for line in file:
+        line = line.strip()
+        line, comment = inline_comments(line)
 
-for line in lines:
-    line = line.strip()
-    line, comment = inline_comments(line)
-
-    for pattern, handler in command_handler.items():
-        if re.match(pattern, line):
-            print_line = handler(line) + comment
-            print(print_line)
-            break
-    else:
-        if line == '#!/bin/dash':
-            line = '#!/usr/bin/python3 -u'
-        print(line)
+        for pattern, handler in command_handler.items():
+            if re.match(pattern, line):
+                print_line = handler(variables, line) + comment
+                print(print_line)
+                break
+        else:
+            if line == '#!/bin/dash':
+                line = '#!/usr/bin/python3 -u'
+            print(line)
